3bc69379d6495d3e4aaff8a861c15ea517777d73
owen970517 <tamia123123@naver.com>
2023-03-25 17:09:54 +0900

    백준 실버 문제 풀이

--- 백준/간판.py
+++ 백준/간판.py
@@ -1,3 +1,29 @@
+# n = int(input())
+# new_name = input()
+# li = []
+# dis_li =[]
+# cnt = 0
+# for i in range(n) :
+#     old_name = input()
+#     if new_name == old_name :
+#         cnt +=1
+#         continue
+#     else :
+#         for j in new_name :
+#             indexes = [idx+1 for idx, c in enumerate(old_name) if c == j]
+#             li.append(indexes)
+# print(li)
+# for i in range(1,len(new_name)) :
+#     print(i , i-1)
+#     dis_li =[]
+#     for j in li[i] :
+#         for k in li[i-1] :
+#             dis = j-k
+#             dis_li.append(dis)
+#     print(dis_li)
+
+
+
 n = int(input())
 new_name = input()
 li = []
@@ -10,14 +36,13 @@ for i in range(n) :
         continue
     else :
         for j in new_name :
-            indexes = [idx for idx, c in enumerate(old_name) if c == j]
-            li.append(indexes)
-print(li)
-for i in range(1,len(new_name)) :
-    print(i , i-1)
-    dis_li =[]
-    for j in li[i] :
-        for k in li[i-1] :
-            dis = j-k
-            dis_li.append(dis)
-    print(dis_li)
+            dic = {}
+            for k in range(len(old_name)) :
+                if j == old_name[k] :
+                    if j not in dic :
+                        dic[j] = [k+1]
+                    else :
+                        dic[j].append(k+1)
+
+            for a in dic :
+                print(a)
--- 백준/세부분.py
+++ 백준/세부분.py
@@ -1,16 +1,23 @@
 s = input()
-li=[]
-for i in range(len(s)) :
-    now = s[:i+1]
-    print('지금',now)
-    print(s[:i+1])
-    print(s[:i+1][-1])
-    print(min(s[:i+1]))
-    if now[-1] == min(now) :
-        mini = now
-        print('최소' , mini)
-        li.append(mini)
-    else :
-        s= s[i::]
-        print('그다음',s)
-print(li)
+result = s
+for i in range(1, len(s)):
+    for j in range(i+1, len(s)):
+        result = min(result, s[:i][::-1] + s[i:j][::-1] + s[j:][::-1])
+print(result)
+
+# 내가 푼 풀이 
+# s = input()
+# li=[]
+# cnt = 0
+# while 1 :
+#     idx = s.find(min(s))
+#     if cnt < 2 :
+#         li.append(s[:idx+1])
+#         cnt += 1
+#         s = s[idx+1::]
+#     elif cnt == 2 :
+#         li.append(s)
+#         break
+# print(li)
+
+# print(li[0][::-1]+li[1][::-1]+li[2][::-1])  
