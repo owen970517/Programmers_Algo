6f9ea000a9d42d96a2c896939db069484125afbe
owen970517 <tamia123123@naver.com>
2023-07-04 18:52:31 +0900

    알고리즘 문제 풀기

--- 백준/치즈.py
+++ 백준/치즈.py
@@ -1,29 +1,43 @@
-def dfs(x,y) :
-    visited[x][y] = True
-    for i in range(4) :
-        nx = x +dx[i]
-        ny = y +dy[i]
-        if nx < 0 or nx>=len(graph) or ny < 0 or ny>=len(graph[0]):
-            continue
-        if graph[nx][ny] != 0 :
-            graph[nx][ny] += 1
-        else :
-            visited[nx][ny]=True
-            dfs(nx,ny)
+from collections import deque
+
+def bfs() :
+    q = deque()
+    q.append((0,0))
+    visited[0][0] = True
+    cheeze = 0
+    while q :
+        x,y = q.popleft()
+        for i in range(4) :
+            nx = x +dx[i]
+            ny = y +dy[i]
+            if 0<= nx < n and 0 <= ny < m and not visited[nx][ny] :
+                visited[nx][ny] = True
+                if graph[nx][ny] == 0 :
+                    q.append((nx,ny))
+                if graph[nx][ny] == 1 :
+                    graph[nx][ny] = 0
+                    cheeze += 1
+    return cheeze
+
 
 n,m = map(int,input().split())
 graph = []
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 total = 0
-visited = [[False] * m for _ in range(n)]
+time = 1
 for i in range(n) : 
     s = list(map(int,input().split()))
     graph.append(s)
-print(graph)
-for i in range(n) :
-    hour= 0
-    for j in range(m) :
-        if graph[i][j] == 1:
-            dfs(i,j)    
-print(graph)
+    total += s.count(1)
+while 1 :
+    visited = [[False] * m for _ in range(n)]
+    now = bfs()
+    print(now)
+    total -= now
+    if total == 0 :
+        print(time)
+        print(now)
+        break
+    time += 1
+    
---
+++ 백준/톱니바퀴.py
@@ -0,0 +1,93 @@
+from collections import deque
+from copy import deepcopy
+
+# def move(num,d) :
+#     global cur_right,cur_left
+#     now_d = d
+#     for i in reversed(range(num-1)) :
+#         if gear[i][right] != cur_left:
+#             cur_left = gear[i][left]
+#             if num % 2 == 1:
+#                 if i % 2 == 0 :
+#                     gear[i].rotate(d)
+#                 else :
+#                     gear[i].rotate(dic[d])
+#             else :
+#                 if i % 2 == 0 :
+#                     gear[i].rotate(dic[d])
+#                 else :
+#                     gear[i].rotate(d)
+#         else :
+#             break
+
+#     d = now_d
+#     for i in range(num,t) :
+#         if gear[i][left] != cur_right :
+#             cur_right = gear[i][right]
+#             if num % 2 == 1:
+#                 if i % 2 == 0 :
+#                     gear[i].rotate(d)
+#                 else :
+#                     gear[i].rotate(dic[d])
+#             else :
+#                 if i % 2 == 0 :
+#                     gear[i].rotate(dic[d])
+#                 else :
+#                     gear[i].rotate(d)
+#         else :
+#             break
+
+t = int(input())
+gear = []
+dic = {
+    1 : -1,
+    -1 : 1
+}
+for i in range(t) :
+    s = deque(map(int,input()))
+    gear.append(s)
+
+k = int(input())
+for i in range(k) :
+    num,d = map(int,input().split())
+    left,right = 6,2
+    now = deepcopy(gear[num-1])
+    cur_left,cur_right = gear[num-1][left],gear[num-1][right]
+    gear[num-1].rotate(d)
+
+    for i in reversed(range(num-1)):
+        if gear[i][right] != cur_left:
+            cur_left = gear[i][left]
+            if num % 2 == 1:
+                if i % 2 == 0 :
+                    gear[i].rotate(d)
+                else :
+                    gear[i].rotate(dic[d])
+            else :
+                if i % 2 == 0 :
+                    gear[i].rotate(dic[d])
+                else :
+                    gear[i].rotate(d)
+        else :
+            break
+    for i in range(num,t) :
+        if gear[i][left] != cur_right :
+            cur_right = gear[i][right]
+            if num % 2 == 1 :
+                if i % 2 == 0 :
+                    gear[i].rotate(d)
+                else :
+                    gear[i].rotate(dic[d])
+            else :
+                if i % 2 == 0 :
+                    gear[i].rotate(dic[d])
+                else :
+                    gear[i].rotate(d)
+        else :
+            break
+        
+ans = 0
+for i in gear :
+    if i[0] == 1 :
+        ans += 1
+print(ans)
