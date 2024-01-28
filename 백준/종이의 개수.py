cecb8e516f3dd2b946b0a186d779533727bb90f0
owen970517 <tamia123123@naver.com>
2024-01-28 16:53:29 +0900

    백준 문제풀기

--- 백준/종이의 개수.py
+++ 백준/종이의 개수.py
@@ -1,29 +1,30 @@
 def quad_tree(x,y,n) :
-    global blue,white
+    global a,b,c
     now = li[x][y]
 
     for i in range(x,x+n) :
         for j in range(y,y+n) :
             if li[i][j] != now :
-                quad_tree(x,y,n//2)
-                quad_tree(x,y+n//2,n//2)
-                quad_tree(x+n//2,y,n//2)
-                quad_tree(x+n//2,y+n//2,n//2)
+                for k in range(3) :
+                    for l in range(3) :
+                        quad_tree(x+k*n//3,y+l*n//3,n//3)
                 return
-    if now == 0 :
-        white += 1
+    if now == -1 :
+        a += 1
+    elif now == 0 :
+        b += 1
     else :
-        blue += 1
+        c += 1
 
 n = int(input())
 li = []
-blue = 0
-white = 0
+a,b,c = 0,0,0
 
 for _ in range(n) :
     s = list(map(int,input().split()))
     li.append(s)
 
 quad_tree(0,0,n)
-print(white)
-print(blue)
+print(a)
+print(b)
+print(c)
